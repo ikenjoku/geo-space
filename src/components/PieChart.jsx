5665e8a8023c5d40bd56700969ce7346694c0cbe
Ike Njoku <ikeenjoku@gmail.com>
2020-10-19 19:44:35 +0100

    add pie chart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Chart, ChartContainer } from './styles'
+import { PieChart as ReChart, Pie, Tooltip } from 'recharts'
+import { ChartContainer } from './styles'
+import { getChartData } from '../utils'
 
-export default function PieChart() {
+export default function PieChart({ chartData }) {
+  const data = getChartData(chartData)
   return (
     <ChartContainer>
-      <Chart></Chart>
+      <ReChart width={200} height={200} >
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={data}
+          nameKey="name"
+          dataKey="value"
+          outerRadius={50}
+          fill="#8884d8"
+          label={true}
+        />
+        <Tooltip />
+      </ReChart>
     </ChartContainer>
   )
 }
